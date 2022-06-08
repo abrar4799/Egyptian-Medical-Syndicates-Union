8fd1947c8f51ef9f92c4980abb67575866469b7d
abrar4799 <abrar.it99@gmail.com>
2022-06-08 13:34:35 +0200

    Done

---
+++ main.py
@@ -0,0 +1,7 @@
+numOne = int(input("Enter Fisrt Number "))
+numTwo = int(input("Enter Second Number "))
+sum = numOne + numTwo
+Subtract = numOne - numTwo
+multiply = numOne * numTwo
+division = numOne / numTwo
+print("sum = " , sum , "Subtract = " , Subtract , "multiply = " , multiply , "division = " , division)
